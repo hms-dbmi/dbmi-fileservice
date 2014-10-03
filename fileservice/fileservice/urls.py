e6ed7874ba7cd6dbf2e85953ecbc8db0a456f13b
David Bernick <dbernick@gmail.com>
2014-10-02 21:06:14 -0400

    initial seed

---
+++ fileservice/fileservice/urls.py
@@ -0,0 +1,25 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'', include('social_auth.urls')),
+    #url(r'^secrets/',include('secrets.urls')),
+    url(r'^api-token-auth/', 'rest_framework.authtoken.views.obtain_auth_token'),    
+
+)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
+
