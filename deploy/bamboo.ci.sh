168bccf0249684d9ccd2dfcd2d34b1f51e0ac3fd
David Bernick <dbernick@gmail.com>
2014-12-15 11:36:54 -0500

    fix

--- deploy/bamboo.ci.sh
+++ deploy/bamboo.ci.sh
@@ -2,25 +2,35 @@
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 
 install() {
- apt-get update
- apt-get install -y libpq-dev wget
- apt-get install -y python-dev
- apt-get install -y libxml2-dev
- apt-get install -y libxslt1-dev
- apt-get install -y python-pip
- apt-get install -y zip
- apt-get install -y python-setuptools
+ yum -y update
+ yum install -y postgresql-devel
+ yum install -y wget
+ yum install -y python-devel
+ yum install -y libxml2-devel
+ yum install -y libxslt-devel
+ yum install -y python-pip
+ yum install -y zip
+ yum install -y python-setuptools
  pip install --upgrade awscli
- wget -qO - https://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
- add-apt-repository "deb http://packages.elasticsearch.org/elasticsearch/1.4/debian stable main"
- apt-get update && apt-get install elasticsearch
- update-rc.d elasticsearch defaults 95 10
+ yum install -y mysql-client
+ rpm --import https://packages.elasticsearch.org/GPG-KEY-elasticsearch
+ cat <<< '
+[elasticsearch-1.4]
+name=Elasticsearch repository for 1.4.x packages
+baseurl=http://packages.elasticsearch.org/elasticsearch/1.4/centos
+gpgcheck=1
+gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
+enabled=1' > /etc/yum.repos.d/elasticsearch.repo
+ yum install -y elasticsearch
+ chkconfig --add elasticsearch
+ export JAVA_HOME=/usr/lib/jvm/jre-1.7.0
+ /etc/init.d/elasticsearch restart
 }
 
 install >/tmp/startup.log 2>&1
 
 cd ${BAMBOODIR}
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 cd fileservice
 ./manage.py test filemaster --settings fileservice.settings.local
 cd ${BAMBOODIR}
