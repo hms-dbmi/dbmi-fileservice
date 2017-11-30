78bb12dec24c3f2d72531fdb3f7674f27e60a2de
Bryan Larson <bryan_larson@hms.harvard.edu>
2017-11-30 13:40:46 -0500

    PPM-243 - Removed AWS credentials from configurations

--- deploy/gunicorn-nginx-entry.sh
+++ deploy/gunicorn-nginx-entry.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
+export SECRET_KEY=$(aws ssm get-parameters --names $PS_PATH.django_secret --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export ALLOWED_HOSTS=$(aws ssm get-parameters --names $PS_PATH.allowed_hosts --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export ADMIN_EMAILS=$(aws ssm get-parameters --names $PS_PATH.admin_emails --with-decryption --region us-east-1 | jq -r '.parameters[].value')
+export AWS_S3_UPLOAD_BUCKET=$(aws ssm get-parameters --names $PS_PATH.aws_s3_upload_bucket --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 
 export AUTH0_DOMAIN=$(aws ssm get-parameters --names $PS_PATH.auth0_domain --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export AUTH0_CLIENT_ID=$(aws ssm get-parameters --names $PS_PATH.auth0_client_id --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export AUTH0_SECRET=$(aws ssm get-parameters --names $PS_PATH.auth0_secret --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export AUTH0_CLIENT_SECRET=$(aws ssm get-parameters --names $PS_PATH.auth0_client_secret --with-decryption --region us-east-1 | jq -r '.parameters[].value')
+export AUTH0_SUCCESS_URL=$(aws ssm get-parameters --names $PS_PATH.auth0_success_url --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export AUTH0_CALLBACK_URL=$(aws ssm get-parameters --names $PS_PATH.auth0_callback_url --with-decryption --region us-east-1 | jq -r '.parameters[].value')
+export ACCOUNT_SERVER_URL=$(aws ssm get-parameters --names $PS_PATH.account_server_url --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 
 export MYSQL_HOST=$(aws ssm get-parameters --names $PS_PATH.mysql_host --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export MYSQL_NAME=$(aws ssm get-parameters --names $PS_PATH.mysql_name --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export MYSQL_USER=$(aws ssm get-parameters --names $PS_PATH.mysql_user --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 export MYSQL_PASSWORD=$(aws ssm get-parameters --names $PS_PATH.mysql_password --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 
-export AWS_S3_ACCESS_KEY_ID=$(aws ssm get-parameters --names $PS_PATH.aws_s3_access_key_id --with-decryption --region us-east-1 | jq -r '.parameters[].value')
-export AWS_S3_SECRET_ACCESS_KEY=$(aws ssm get-parameters --names $PS_PATH.aws_s3_secret_access_key --with-decryption --region us-east-1 | jq -r '.parameters[].value')
-export AWS_GLACIER_ACCESS_KEY_ID=$(aws ssm get-parameters --names $PS_PATH.aws_glacier_access_key_id --with-decryption --region us-east-1 | jq -r '.parameters[].value')
-export AWS_GLACIER_SECRET_ACCESS_KEY=$(aws ssm get-parameters --names $PS_PATH.aws_glacier_secret_access_key --with-decryption --region us-east-1 | jq -r '.parameters[].value')
-export AWS_S3_UPLOAD_BUCKET=$(aws ssm get-parameters --names $PS_PATH.aws_s3_upload_bucket --with-decryption --region us-east-1 | jq -r '.parameters[].value')
 
 # Get SSL certs and prepare nginx
 SSL_KEY=$(aws ssm get-parameters --names $PS_PATH.ssl_key --with-decryption --region us-east-1 | jq -r '.Parameters[].Value')
