fd1bfd5b542d06bdc6011f4fbc46c201eda7aab0
Bryan Larson <b32147@gmail.com>
2020-03-25 13:54:48 -0400

    DBMISVC-47 - Re-enabled support for upload/PUT endpoint in API and CLI; configured to allow for both configuration types

--- fileservice/filemaster/files.py
+++ fileservice/filemaster/files.py
@@ -378,13 +378,16 @@ class ArchiveFileList(viewsets.ModelViewSet):
             return HttpResponseForbidden()
 
         # Pull request parameters
-        expires = int(self.request.query_params.get('expires', '10'))
+        try:
+            expires = int(self.request.query_params.get('expires', '10'))
+        except ValueError:
+            return HttpResponseBadRequest(f'Parameter for expiration is not a valid integer')
         bucket = self.request.query_params.get('bucket')
 
         # If no bucket specified, default to first created
         if not bucket:
             try:
-                bucket = next(settings.BUCKETS)
+                bucket = next(iter(settings.BUCKETS))
             except Exception as e:
                 log.exception(f'Error finding default bucket: {e}', exc_info=True, extra={'request': request})
                 return HttpResponseBadRequest(f'No default bucket has been configured for Fileservice, must specify'
@@ -402,7 +405,6 @@ class ArchiveFileList(viewsets.ModelViewSet):
         try:
             conditions_b64 = self.request.query_params.get('conditions')
             if conditions_b64:
-
                 # Decode and load
                 conditions = json.loads(base64.b64decode(conditions_b64.encode()).decode())
 
@@ -592,7 +594,7 @@ class ArchiveFileList(viewsets.ModelViewSet):
 
         try:
             # Fetch the files
-            archivefiles = ArchiveFile.objects.filter(uuid__in=uuids, deletedate__isnull=True, locations__isnull=False)
+            archivefiles = ArchiveFile.objects.filter(uuid__in=uuids, locations__isnull=False)
         except:
             return HttpResponseNotFound()
 
@@ -607,7 +609,6 @@ class ArchiveFileList(viewsets.ModelViewSet):
         # get presigned urls and prepare response body
         body = ''
         for archivefile in archivefiles_allowed:
-
             # Get tje location
             location = archivefile.locations.first()
 
@@ -625,7 +626,7 @@ class ArchiveFileList(viewsets.ModelViewSet):
             body += f'- {location.filesize} {proxy_url} {archivefile.filename}\n'
 
         # Forward it on
-        log.debug('Archive request: \n\n{}\n\n'.format(body))
+        log.debug('File archive request body:\n{}'.format(body))
 
         # Check for specified filename
         if request.query_params.get('filename'):
@@ -676,6 +677,69 @@ class ArchiveFileList(viewsets.ModelViewSet):
         log.debug(f'Sending user to S3 proxy: {response["X-Accel-Redirect"]}')
 
         return response
+
+    @action(detail=True, methods=['get'], permission_classes=[DjangoObjectPermissionsAll])
+    def upload(self, request, uuid=None):
+        from filemaster.aws import signedUrlUpload
+
+        try:
+            archivefile = ArchiveFile.objects.get(uuid=uuid)
+        except:
+            return HttpResponseNotFound()
+
+        if not request.user.has_perm('filemaster.upload_archivefile', archivefile):
+            return HttpResponseForbidden()
+
+        bucket = self.request.query_params.get('bucket')
+        aws_key = self.request.query_params.get('aws_key', None)
+        aws_secret = self.request.query_params.get('aws_secret', None)
+
+        # If no bucket specified, default to first created
+        if not bucket:
+            try:
+                bucket = next(settings.BUCKETS)
+            except Exception as e:
+                log.exception(f'Error finding default bucket: {e}', exc_info=True, extra={'request': request})
+                return HttpResponseBadRequest(f'No default bucket has been configured for Fileservice, must specify'
+                                              f'bucket in request')
+
+        try:
+            # Check bucket perms
+            if not request.user.has_perm('filemaster.write_bucket', Bucket.objects.get(name=bucket)):
+                return HttpResponseForbidden(f'User does not have permissions on Bucket "{bucket}"')
+        except Bucket.DoesNotExist:
+            return HttpResponseNotFound(f'Bucket "{bucket}" does not exist in Fileservice')
+
+        # Ensure we have a configured IAM User
+        if (not aws_key or not aws_secret) and (not hasattr(settings, 'BUCKET_CREDENTIALS')
+                                                or not settings.BUCKET_CREDENTIALS.get(bucket)
+                                                or not settings.BUCKET_CREDENTIALS[bucket].get('AWS_KEY_ID')
+                                                or not settings.BUCKET_CREDENTIALS[bucket].get('AWS_SECRET')):
+            log.error(f'Upload failed as IAM/STS credentials are not enabled for this instance of Fileservice', extra={
+                'request': request, 'archivefile': archivefile, 'bucket': bucket,
+            })
+            return HttpResponseBadRequest(f'No IAM credentials configured for bucket "{bucket}"')
+
+        urlhash = signedUrlUpload(archivefile, bucket=bucket, aws_key=aws_key, aws_secret=aws_secret)
+
+        url = urlhash["url"]
+        message = "PUT to this url"
+        location = urlhash["location"]
+        locationid = urlhash["locationid"]
+
+        # get presigned url
+        return Response({
+            'url': url,
+            'message': message,
+            'location': location,
+            'locationid': locationid,
+            'bucket': urlhash['bucket'],
+            'foldername': urlhash['foldername'],
+            'filename': urlhash['filename'],
+            'secretkey': urlhash['secretkey'],
+            'accesskey': urlhash['accesskey'],
+            'sessiontoken': urlhash['sessiontoken']
+        })
 
 
 class DownloadLogList(generics.ListAPIView):
--- fileservice/fileservice/settings.py
+++ fileservice/fileservice/settings.py
@@ -3,6 +3,9 @@
 
 from os.path import abspath, basename, dirname, join, normpath
 from sys import path, stdout
+import os
+import warnings
+import logging
 
 from dbmi_client import environment
 
@@ -230,6 +233,7 @@ DBMI_CLIENT_CONFIG = {
 
     # Optionally disable logging
     'ENABLE_LOGGING': True,
+    'LOG_LEVEL': environment.get_int('DBMI_LOG_LEVEL', default=logging.WARNING),
 
     # Universal login screen branding
     'AUTHN_TITLE': 'DBMI Fileservice',
@@ -273,12 +277,47 @@ REST_FRAMEWORK = {
 
 ########## AWS S3 CONFIGURATION
 
-# Set the S3 bucket(s) to enable
-BUCKETS = environment.get_list('AWS_S3_BUCKETS', default=[])
-if not BUCKETS:
+# Check for specified buckets
+if os.environ.get('DBMI_S3_BUCKETS'):
+    BUCKETS = environment.get_list('DBMI_S3_BUCKETS', default=[])
 
-    # The old configuration had bucke names as keys (credentials as values that are no longer needed)
-    BUCKETS += environment.get_dict('AWS_S3_BUCKETS', default={}).keys()
+# Check for deprecated configuration
+else:
+    BUCKET_CREDENTIALS = {}
+
+    if os.environ.get('AWS_S3_UPLOAD_BUCKET') and os.environ.get('AWS_STS_ACCESS_KEY_ID') \
+        and os.environ.get('AWS_STS_SECRET_ACCESS_KEY'):
+
+        # Set the default S3 bucket to use when not specified
+        S3_DEFAULT_BUCKET = environment.get_str('AWS_S3_UPLOAD_BUCKET')
+        AWS_STS_ACCESS_KEY_ID = environment.get_str('AWS_STS_ACCESS_KEY_ID')
+        AWS_STS_SECRET_ACCESS_KEY = environment.get_str('AWS_STS_SECRET_ACCESS_KEY')
+
+        BUCKET_CREDENTIALS.update({
+            S3_DEFAULT_BUCKET: {
+                'AWS_KEY_ID': AWS_STS_ACCESS_KEY_ID,
+                'AWS_SECRET': AWS_STS_SECRET_ACCESS_KEY
+            }
+        })
+
+    if os.environ.get('AWS_S3_BUCKETS'):
+        BUCKET_CREDENTIALS.update({
+            bucket: {
+                'AWS_KEY_ID': credentials.get('AWS_KEY_ID'),
+                'AWS_SECRET': credentials.get('AWS_SECRET'),
+            } for bucket, credentials in environment.get_dict('AWS_S3_BUCKETS').items()
+        })
+
+    # Retain list of buckets for updated settings configuration
+    BUCKETS = list(BUCKET_CREDENTIALS.keys())
+
+    warnings.warn(
+        'Fileservice configurations with AWS IAM user credentials should be avoided',
+        DeprecationWarning
+    )
+if not BUCKETS:
+    raise SystemError(f'Invalid configuration: DBMI_S3_BUCKETS or AWS_S3_UPLOAD_BUCKET/AWS_STS_ACCESS_KEY_ID/'
+                      f'AWS_STS_SECRET_ACCESS_KEY and/or AWS_S3_BUCKETS with credentials must be defined')
 
 ########## END AWS S3 CONFIGURATION
 
@@ -335,6 +374,11 @@ LOGGING = {
             'propagate': False,
         },
         'botocore': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+            'propagate': True,
+        },
+        'boto': {
             'level': 'WARNING',
             'handlers': ['console'],
             'propagate': True,
