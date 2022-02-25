2189f4facf749480562264a831f2a60480494c4c
John Carmichael <jlcarmic@gmail.com>
2022-02-25 10:16:09 -0500

    Add a healthcheck route

--- fileservice/fileservice/urls.py
+++ fileservice/fileservice/urls.py
@@ -4,13 +4,15 @@ from django.core.exceptions import PermissionDenied
 from django.conf.urls import url, include
 from django.views.defaults import permission_denied
 
+from .views import Healthcheck
+
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/login/', permission_denied, {'exception': PermissionDenied('Permission Denied')}),
     url(r'^admin/', admin.site.urls, name='admin'),
-    url(r'^filemaster/',include('filemaster.urls', namespace='filemaster')),
-    url(r'^healthcheck/?', include('health_check.urls')),
+    url(r'^filemaster/', include('filemaster.urls', namespace='filemaster')),
+    url(r'^healthcheck/?', Healthcheck.as_view(), name='healthcheck'),
     url(r'^dbmi-auth/', include('dbmi_client.login.urls')),
     url(r'^$', lambda r: redirect(reverse('filemaster:index'))),
 ]
